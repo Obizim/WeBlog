e5b1be6a2e91efa9c77261d216ebb6c1d4ff19d1
Obi Zim <obizimuzo@gmail.com>
2020-08-12 01:50:17 -0700

    Form
    Add comment and form validation

--- src/views/BlogDetails.vue
+++ src/views/BlogDetails.vue
@@ -1,48 +1,154 @@
 <template>
   <div class="single-blog">
-    <h1 v-rainbow>{{ blog.title }}</h1>
+    <h1 v-rainbow>{{ blog.title | uppercase }}</h1>
     <p>{{ blog.body }}</p>
+
+    <p class="comment">Comments:</p>
+    <section>
+      <div>
+        <div class="comments" v-for="(comment, index) in comments" :key="index">
+          <h3 v-rainbow>{{ comment.email }}</h3>
+          <article v-rainbow>{{ comment.body | reduce }}</article>
+        </div>
+      </div>
+      <form>
+        <h3>Add Comment</h3>
+        <label for="email">Email Address: </label>
+        <input type="text" v-model="details.email" required />
+        <br />
+        <label for="body">Body: </label>
+        <input type="text" v-model="details.body" required />
+        <button @click.prevent="validateForm">Submit</button>
+      </form>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       id: this.$route.params.id,
-      blog: {}
-    }
+      blog: {},
+      comments: [],
+      details: {
+        body: " ",
+        email: " "
+      }
+    };
   },
   created() {
-    fetch('https://jsonplaceholder.typicode.com/posts/' + this.id, {
-      method: 'GET'
+    fetch("https://jsonplaceholder.typicode.com/posts/" + this.id, {
+      method: "GET"
     })
-      .then((response) => {
-        return response.json()
+      .then(response => {
+        return response.json();
       })
       .then(jsonData => {
-        this.blog = jsonData
+        this.blog = jsonData;
+      });
+    fetch("https://jsonplaceholder.typicode.com/comments", {
+      method: "GET"
+    })
+      .then(response => {
+        return response.json();
       })
+      .then(jsonData => {
+        this.comments = jsonData.slice(0, 4);
+      });
+  },
+  methods: {
+    validateForm() {
+      if (this.details.email && this.details.body == " ") {
+        alert("Email address and body cannot be empty");
+      } else {
+        this.comments.push(this.details);
+        this.details = {};
+      }
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.single-blog{
+.single-blog {
   margin: 30px auto;
   max-width: 900px;
   padding: 20px 10px;
   background: #eee;
   border-radius: 5px;
 
-  h1{
+  h1 {
     background: transparent;
     margin: 10px 10px;
+    font-size: 1.5rem;
   }
-  p{
+  p {
     background: transparent;
     color: black;
     margin: 30px 10px;
+  }
+}
+.comment {
+  text-align: left;
+  font-size: 1rem;
+  font-weight: 700;
+}
+
+section {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.comments {
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+  align-items: flex-start;
+  justify-content: center;
+
+  h3 {
+    font-size: 1rem;
+    margin: 10px 0;
+  }
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
+  h3 {
+    margin-bottom: 20px;
+  }
+
+  input {
+    border: 1px solid #fff;
+    height: 30px;
+    width: 250px;
+    margin-top: 10px;
+    padding: 5px;
+  }
+  button {
+    border: none;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    padding: 10px 20px;
+    background: rgb(3, 4, 94);
+    outline: none;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  section {
+    flex-direction: column;
+  }
+  form {
+    align-items: center;
   }
 }
 </style>
--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,66 +1,67 @@
 <template>
   <div class="show-blogs">
-     <h1>{{ title }}</h1>
+    <h1>{{ title }}</h1>
     <div class="single-blog" v-for="(blog, index) in blogs" :key="index">
-       <!-- <img :src="blog.strCategoryThumb" class="img-circle" alt="recipe picture" /> -->
-      <router-link :to="'/blog/' + blog.id"><h2 v-rainbow>{{ blog.title | to-uppercase }}</h2></router-link>
+      <router-link :to="'/blog/' + blog.id"
+        ><h2 v-rainbow>{{ blog.title | uppercase }}</h2></router-link
+      >
       <article v-rainbow>{{ blog.body | reduce }}</article>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  data () {
+  name: "Home",
+  data() {
     return {
-      title: 'Trending blogs',
+      title: "Trending blogs",
       blogs: []
-    }
+    };
   },
-  created () {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'GET'
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "GET"
     })
-      .then((response) => {
-        return response.json()
+      .then(response => {
+        return response.json();
       })
       .then(jsonData => {
-        this.blogs = jsonData
-      })
+        this.blogs = jsonData;
+      });
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.show-blogs{
+.show-blogs {
   max-width: 800px;
-  margin:30px auto;
+  margin: 30px auto;
   padding: 0 10px;
 
-  h1{
+  h1 {
     text-align: left;
-    font-size: 1rem;
+    font-size: 1.5rem;
+    font-weight: 500;
   }
 }
-.single-blog{
+.single-blog {
   padding: 20px;
   margin: 20px 0;
   box-sizing: border-box;
   background: #eee;
   border-radius: 5px;
 }
-a{
+a {
   text-decoration: none;
 }
-h2{
+h2 {
   color: #000;
   background: transparent;
   font-size: 1rem;
   font-weight: 400;
 }
-article{
+article {
   background: transparent;
   margin-top: 10px;
 }
